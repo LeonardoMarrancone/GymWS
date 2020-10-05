c24ab6dc10bac028581f2319283a7c7aa959e50f
AposVinc <apos.vincenzo@gmail.com>
2020-10-05 09:38:00 +0200

    .

--- src/main/java/it/univaq/disim/gymws/GymWSImpl.java
+++ src/main/java/it/univaq/disim/gymws/GymWSImpl.java
@@ -5,6 +5,7 @@
  */
 package it.univaq.disim.gymws;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -24,82 +25,86 @@ import wsdl.gymrest.disim.univaq.it.gymportalws.TipoUtente;
 @WebService(serviceName = "GymPortalWSMain", portName = "GymPortalWSSOAP", endpointInterface = "wsdl.gymrest.disim.univaq.it.gymportalws.GymPortalWSInterface", targetNamespace = "http://it.univaq.disim.GymREST.wsdl/GymPortalWS.wsdl", wsdlLocation = "WEB-INF/wsdl/GymPortalWS.wsdl")
 public class GymWSImpl {
 
-    private static final String urlDB = "jdbc:mysql://127.0.0.1:8889/gymportal?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String urlDB = "jdbc:mysql://127.0.0.1:8889/gymportal_db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     private static final String userDB = "gymportal";
     private static final String pswDB = "gymportal";
-    
+
+
+    public static final String GET_UTENTI= "SELECT * FROM user";
+    public static final String ADD_RUOLO_UTENTE= "UPDATE user_role SET role_id=? WHERE user_id=?";
     public static final String GET_UTENTE = "SELECT * FROM user WHERE id=?";
-    public static final String GET_UTENTI= "SELECT * FROM user"; 
-    public static final String GET_RUOLI= "SELECT * FROM role"; 
-    public static final String UPDATE_RUOLO_UTENTE= "UPDATE user_role SET role_id=? WHERE user_id=?";
-        
-    
+    public static final String GET_RUOLI= "SELECT * FROM role";
+
+
     public wsdl.gymrest.disim.univaq.it.gymportalws.TipoListaUtente getUtenti() throws MsgErrore {
-       
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             System.exit(1);
         }
-            
+
         TipoListaUtente listaUtenti = new TipoListaUtente();
         try (Connection connection = DriverManager.getConnection(urlDB,userDB,pswDB);
              Statement st = connection.createStatement();
              ResultSet rs = st.executeQuery(GET_UTENTI);) {
 
-                while (rs.next()){
-                    TipoUtente utente = new TipoUtente();
-                    utente.setId(rs.getLong(1));
-                    utente.setEmail(rs.getString(3));
-                    utente.setLastName(rs.getString(4));
-                    utente.setName(rs.getString(5));
-                    utente.setPassword(rs.getString(6));
-                    utente.setUserName(rs.getString(7));
-                    
-                    listaUtenti.getUtente().add(utente);
-                }
-        
+            while (rs.next()){
+
+                TipoUtente utente = new TipoUtente();
+                utente.setId(rs.getLong(1));
+                utente.setEmail(rs.getString(2));
+                utente.setLastname(rs.getString(3));
+                utente.setName(rs.getString(4));
+                utente.setPassword(rs.getString(5));
+                utente.setUsername(rs.getString(6));
+
+                listaUtenti.getUtente().add(utente);
+            }
+
         } catch (SQLException ex) {
             Logger.getLogger(GymWSImpl.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return listaUtenti;
-        
-        
+
     }
-    
-    public void updateRuoloUtente(long idUtente, long idRuolo) throws MsgErrore {
-        
+
+    public void addRuoloUtente(long idUtente, long idRuolo) throws MsgErrore {
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             System.exit(1);
         }
+
         try (Connection connection = DriverManager.getConnection(urlDB,userDB,pswDB);
-             PreparedStatement st = connection.prepareStatement(UPDATE_RUOLO_UTENTE);) {
-            st.setLong(1, idRuolo);
+             PreparedStatement st = connection.prepareStatement(ADD_RUOLO_UTENTE);) {
+
             st.setLong(2, idUtente);
+            st.setLong(1, idRuolo);
 
             st.execute();
 
         } catch (SQLException e) {
             e.printStackTrace();
-        }  
-        
+        }
+
     }
 
     public wsdl.gymrest.disim.univaq.it.gymportalws.TipoUtente getUtente(long id) throws MsgErrore {
-        
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             System.exit(1);
         }
-        
+
         TipoUtente utente = new TipoUtente();
+
         try (Connection connection = DriverManager.getConnection(urlDB,userDB,pswDB);
              PreparedStatement st = connection.prepareStatement(GET_UTENTE);) {
             st.setLong(1,id);
@@ -107,11 +112,11 @@ public class GymWSImpl {
             try (ResultSet rs = st.executeQuery();) {
                 if (rs.next()) {
                     utente.setId(rs.getLong(1));
-                    utente.setEmail(rs.getString(3));
-                    utente.setLastName(rs.getString(4));
-                    utente.setName(rs.getString(5));
-                    utente.setPassword(rs.getString(6));
-                    utente.setUserName(rs.getString(7));
+                    utente.setEmail(rs.getString(2));
+                    utente.setLastname(rs.getString(3));
+                    utente.setName(rs.getString(4));
+                    utente.setPassword(rs.getString(5));
+                    utente.setUsername(rs.getString(6));
                 }
             }
 
@@ -119,7 +124,7 @@ public class GymWSImpl {
             e.printStackTrace();
         }
         return utente;
-        
+
     }
 
     public wsdl.gymrest.disim.univaq.it.gymportalws.TipoListaRuolo getRuoli() throws MsgErrore {
@@ -129,25 +134,26 @@ public class GymWSImpl {
             e.printStackTrace();
             System.exit(1);
         }
-            
+
         TipoListaRuolo listaRuoli = new TipoListaRuolo();
         try (Connection connection = DriverManager.getConnection(urlDB,userDB,pswDB);
              Statement st = connection.createStatement();
              ResultSet rs = st.executeQuery(GET_RUOLI);) {
-            
+
             while (rs.next()){
                 TipoRuolo ruolo = new TipoRuolo();
                 ruolo.setId(rs.getLong(1));
                 ruolo.setRole(rs.getString(2));
-                    
+
                 listaRuoli.getRuolo().add(ruolo);
             }
-        
+
         } catch (SQLException ex) {
             Logger.getLogger(GymWSImpl.class.getName()).log(Level.SEVERE, null, ex);
         }
+
         return listaRuoli;
-        
+
     }
-    
+
 }
